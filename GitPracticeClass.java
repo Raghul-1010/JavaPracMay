fb3bb15aa3261e32a0a71e32e8d08f4ecac1fe9e
TestAccountSR <testframework157@gmail.com>
2025-05-24 20:21:43 +0530

    Decalred Variable with data types and printed using Sout Statement-3

--- GitPracticeClass.java
+++ GitPracticeClass.java
@@ -3,6 +3,8 @@ public class GitPracticeClass {
         int a = 10;
         int b = 30;
         int f= a*b;
+        int g = b-a;
         System.out.println(f);
+        System.out.println(g);
     }
 }
