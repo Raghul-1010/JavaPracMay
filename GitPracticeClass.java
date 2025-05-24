ae30b48de1e12bd36defe0e4b10b4d933c685f3a
Raghul-1010 <raghul7092@gmail.com>
2025-05-24 21:42:38 +0530

    Changed A to 99

--- GitPracticeClass.java
+++ GitPracticeClass.java
@@ -1,6 +1,6 @@
 public class GitPracticeClass {
     public static void main(String[] args) {
-        int a = 10;
+        int a = 99;
         int b = 40;
         int c = 15;
         int f= a*b;
