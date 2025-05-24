88817868c3477c21c91ea4ece8652bb219f974cf
TestAccountSR <testframework157@gmail.com>
2025-05-24 22:08:01 +0530

    A to 20

--- GitPracticeClass.java
+++ GitPracticeClass.java
@@ -1,11 +1,12 @@
 public class GitPracticeClass {
     public static void main(String[] args) {
-        int a = 99;
+
+        int a = 22;
+
         int b = 40;
         int c = 15;
         int f= a*b;
         int g = b-a;
-        int c=23;
         System.out.println(f);
         System.out.println(g);
     }
